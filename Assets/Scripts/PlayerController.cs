d4f2226fc9f39eafe962351e5c5d14118ad3eb48
Caseycjc <knightlaggia@gmail.com>
2023-06-22 14:30:38 -0500

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public int health;
+    private Rigidbody rb;
+    private int score;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        score = 0;
+        health = 5;
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            score++;
+            Debug.Log("Score: " + score);
+        }
+        else if (other.gameObject.CompareTag("Trap"))
+        {
+            health--;
+            Debug.Log("Health: " + health);
+        }
+        else if (other.gameObject.CompareTag("Goal"))
+        {
+            Debug.Log("You win!");
+        }
+    }
+    void Update()
+    {
+        if (health == 0)
+        {
+            Debug.Log("Game Over!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
