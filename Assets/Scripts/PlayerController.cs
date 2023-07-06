dc70c487c6b0906a232d4ffb9464a91909795439
Caseycjc <knightlaggia@gmail.com>
2023-07-05 19:35:46 -0500

    menu

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,17 +7,20 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-    public int health;
+    public int maxHealth = 5;
     public Text scoreText;
     public Text healthText;
+    public Text winLoseText;
+    public Image winLoseBG;
     private Rigidbody rb;
     private int score;
+    private int health;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         score = 0;
-        health = 5;
+        health = maxHealth;
         SetScoreText();
         SetHealthText();
     }
@@ -43,20 +46,20 @@ public class PlayerController : MonoBehaviour
         else if (other.gameObject.CompareTag("Trap"))
         {
             health--;
+            health = Mathf.Clamp(health, 0, maxHealth); // Ensure health doesn't go below 0
             SetHealthText();
         }
         else if (other.gameObject.CompareTag("Goal"))
         {
-            Debug.Log("You win!");
+            WinCondition();
         }
     }
 
     void Update()
     {
-        if (health == 0)
+        if (health <= 0)
         {
-            Debug.Log("Game Over!");
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            LoseCondition();
         }
     }
 
@@ -68,5 +71,29 @@ public class PlayerController : MonoBehaviour
     void SetHealthText()
     {
         healthText.text = "Health: " + health.ToString();
+    }
+
+    void WinCondition()
+    {
+        winLoseText.text = "You Win!";
+        winLoseBG.color = Color.green;
+        winLoseText.gameObject.SetActive(true);
+        winLoseBG.gameObject.SetActive(true);
+        StartCoroutine(LoadScene(3)); // Start the coroutine when player wins
+    }
+
+    void LoseCondition()
+    {
+        winLoseText.text = "Game Over!";
+        winLoseBG.color = Color.red;
+        winLoseText.gameObject.SetActive(true);
+        winLoseBG.gameObject.SetActive(true);
+        StartCoroutine(LoadScene(3)); // Start the coroutine when player loses
+    }
+
+    IEnumerator LoadScene(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
